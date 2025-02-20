4ea0be6d21a2375948beb28cfa319210a26874cc
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-02-20 17:54:06 +0700

    styled

--- src/Button.jsx
+++ src/Button.jsx
@@ -12,8 +12,12 @@ export const StyledButton = styled.button`
   height: 36px;
   background-color: #0073ff;
   color: white;
+  transition: all 0.3s ease-in-out;
   &:hover {
     background-color: #0073ffc3;
+    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);
+
+    transform: translate(4px, 4px);
   }
 `;
 
